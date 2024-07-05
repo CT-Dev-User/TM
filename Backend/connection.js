6f872858c087f0535626b16461071440d77bf726
ctusername <projects@conscientioustech.in>
2024-07-05 12:53:49 +0530

    seven commit

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,7 +1,20 @@
-import mongoose from "mongoose";
-import dotenv from 'dotenv'
-dotenv.config()
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-export const conn =  mongoose.connect(process.env.MONGODB_URL).then((res)=>{
-    console.log("DB Connected Successfully..!!")
-}) 
+dotenv.config();
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+};
+
+export default connectDB;
