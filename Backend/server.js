13540ffbc5964d6906d57e4c43671421dda593f5
ctusername <projects@conscientioustech.in>
2024-07-05 14:36:53 +0530

    14 commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,29 +1,6 @@
  
 
 
-// import express from 'express';
-// import cors from 'cors';
-// import homeRouter from './routes/homeRoutes.js';
-// import queryformrouter from './controllers/aboutUs/queryFormData.js';
-// import router from './routes/dataRoute.js';
-// import { conn } from './connection.js';
-
-// const app = express();
-
-// // Middleware
-// app.use(express.json()); // Parse JSON bodies
-// app.use(cors()); // Enable CORS
-
-// // Routes
-// app.use(homeRouter);
-// app.use(queryformrouter);
-// app.use(router);
-
-// // Start server
-// const PORT = process.env.PORT || 8000;
-// app.listen(PORT, () => {
-//   console.log(`Server running @ http://localhost:${PORT}`);
-// });
 import express from 'express';
 import cors from 'cors';
 import homeRouter from './routes/homeRoutes.js';
@@ -38,15 +15,11 @@ app.use(express.json()); // Parse JSON bodies
 app.use(cors()); // Enable CORS
 
 // Routes
-// app.use(homeRouter);
-// app.use(queryformrouter);
-// app.use(router);
+app.use(homeRouter);
+app.use(queryformrouter);
+app.use(router);
 
-router.get('/', (req, res) => {
-  res.send('This is an example route');
-});
-
-// Listen on the port provided by Vercel
+// Start server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running @ http://localhost:${PORT}`);
